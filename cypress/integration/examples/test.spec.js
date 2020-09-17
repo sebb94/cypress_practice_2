518ea601f50363aff87d8e2c080fc510c87c6427
sebb94 <omgl0lwth@gmail.com>
2020-09-17 11:23:35 +0200

    jqery and cypress context

--- cypress/integration/examples/test.spec.js
+++ cypress/integration/examples/test.spec.js
@@ -48,6 +48,43 @@ describe('First Suite', () => {
 
   });
 
+  it.only('ten and wrap methods', () => {
+      visitApp()
+
+    //   cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain','Email')
+    //   cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain','Password')
+    //   cy.contains('nb-card', 'Basic Form').find('[for="exampleInputEmail1"]').should('contain','Email Address')
+    //   cy.contains('nb-card', 'Basic Form').find('[for="exampleInputPassword1"]').should('contain','Password')
+
+      // selenium - not working
+
+    //   const firstForm = cy.contains('nb-card', 'Using the Grid')
+    //   const secondForm = cy.contains('Basic form')
+
+    //   firstForm.find('[for="inputEmail1"]').should('contain','Email')
+    //   firstForm.find('[for="inputPassword2"]').should('contain','Password')
+    //   secondForm.find('[for="exampleInputEmail1"]').should('contain','Email Address')
+
+
+      // cypress (jQuery)
+
+      cy.contains('nb-card','Using the Grid').then( firstForm => {
+        // jquery format - cant use cypress
+        const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+        expect(passwordLabelFirst).to.equal('Password')
+
+        cy.contains('nb-card','Basic form').then( secondForm => {
+            // jquery format - cant use cypress
+            const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+            expect(passwordLabelFirst).to.equal(passwordSecondText)
+    
+            // go back to wordpress
+            cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain',passwordSecondText)
+          })
+
+      })
+  });
+
 });
 
 
