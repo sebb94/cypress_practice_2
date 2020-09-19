203ad708ec4b1ea46b909b8ab051370770852e79
sebb94 <omgl0lwth@gmail.com>
2020-09-19 11:37:12 +0200

    e2e - fix

--- cypress/integration/examples/testWithPageObject.spec.js
+++ cypress/integration/examples/testWithPageObject.spec.js
@@ -14,4 +14,17 @@ describe('Navigation test', () => {
             navigateTo.tooltipPage()
        
     });
+
+    it(' should submit Inline and Basic form and select tomorrow date in the calendar', () => {
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail('Artem', 'test@test.com')
+        onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password')
+        navigateTo.datepickerPage()
+        onDatePickerPage.selectCommonDatepickerDateFromToday(1)
+        onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14)
+        navigateTo.smartTablePage()
+        onSmartTablePage.addNewRecordWithFirstAndLastName('Artem', 'Bondar')
+        onSmartTablePage.updateAgeByFirstName('Artem', '35')
+        onSmartTablePage.deleteRowByIndex(1)
+    })
 });
